b4952f140fadc02ee3a3ee9661b3b219f4963d59
AKSHAY H <akshayh2004ak08@gmail.com>
2026-09-17 12:59:49 +0530

    Time: 32 ms (58.21%), Space: 21.7 MB (84.84%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subarraySum(self, nums: list[int], k: int) -> int:
+        count = 0
+        current_sum = 0
+        # Hash map to store (prefix_sum -> frequency)
+        prefix_sums = {0: 1}
+        
+        for num in nums:
+            current_sum += num
+            
+            # If (current_sum - k) exists in our map, 
+            # it means we found subarray(s) that sum to k
+            if current_sum - k in prefix_sums:
+                count += prefix_sums[current_sum - k]
+            
+            # Record the current prefix sum in the map
+            prefix_sums[current_sum] = prefix_sums.get(current_sum, 0) + 1
+            
+        return count
