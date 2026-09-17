5b26bca910c4b2f6f37564d9766c74803e1fe143
AKSHAY H <akshayh2004ak08@gmail.com>
2026-09-17 13:02:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (24.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -6,18 +6,14 @@ class Solution:
         low = 0
         mid = 0
         high = len(nums) - 1
-
+        
         while mid <= high:
             if nums[mid] == 0:
-                # Swap elements at low and mid, then move both pointers forward
                 nums[low], nums[mid] = nums[mid], nums[low]
                 low += 1
                 mid += 1
             elif nums[mid] == 1:
-                # The element is already in the correct relative position, move mid forward
                 mid += 1
-            else:  # nums[mid] == 2
-                # Swap elements at mid and high, decrement high pointer
-                # Do NOT increment mid here because the new nums[mid] needs to be evaluated
+            else: # nums[mid] == 2
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
